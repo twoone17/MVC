f6a0cce1ee5d8b0e72c22728746f800760ba82fd
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-07-07 16:23:33 +0900

    [0707]컴포넌트스캔까지

--- Study/Spring/core/src/main/java/hello/core/AutoAppConfig.java
+++ Study/Spring/core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class)
         //앞서 만들어두었던 설정 정보도 함께 등록되고, 실행되어 버린다 이를 빼준다
         //실무에선 다 componentScan 사용한다
