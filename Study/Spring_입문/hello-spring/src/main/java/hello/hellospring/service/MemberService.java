670e50e27b25a6daf416a5aba5c7c245c03d1ec6
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-07-11 02:27:21 +0900

    [0710]입문편 스프링 빈 등록전까지

--- Study/Spring_입문/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ Study/Spring_입문/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,6 +3,8 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +13,7 @@ public class MemberService {
 
     private MemberRepository memberRepository = new MemoryMemberRepository();
 
+//생성을할때 등록하면서 의존관계도 같이 주입
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
