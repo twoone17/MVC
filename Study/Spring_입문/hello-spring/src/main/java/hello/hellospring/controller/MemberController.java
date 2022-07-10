ca0acf151c4d1dc1adb04455af18c8aa83fe0e44
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-07-11 03:16:40 +0900

    [0710]입문편 회원 웹 기능까지

--- Study/Spring_입문/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ Study/Spring_입문/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,29 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm.html";
+    }
+
+    //postmapping은 뭘까? html method가 post 방식이기 때문에 url은 똑같지만 이게 선택이된다
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        //어떤 방식으로 html id name이 create의 파라미터인 MemberForm로 넘어가는걸까?
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/"; //home화면으로 돌림
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 }
