39235f434b85c3ce82a20707f9f4f210401c141d
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-07-11 18:56:09 +0900

    [0711]JPA 전까지

--- Study/Spring_입문/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ Study/Spring_입문/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity //jpa가 관리하는 애노테이션
 public class Member {
 
+    //db에 insert를 할때 id를 자동으로 생성하는데 이를 identity라고 한다
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
